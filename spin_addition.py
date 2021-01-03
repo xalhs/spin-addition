727971504d12f8388d48000acc914708e2b4b0cc
xalhs <tagarism@gmail.com>
2021-01-03 10:02:40 +0200

    Restructure the code to operate with classes instead of dictionaries

--- spin_addition.py
+++ spin_addition.py
@@ -1,91 +1,134 @@
-import math
-from math import sqrt
-
+#import math
+import sympy
+from sympy import sqrt
+from sympy import*
 
 # we want total to contain the individual small objects
 # and also contain the big objects (irreducible representations)
 # each big object contains states
 
 
-#Total[1][]
-
-#Total = { small_spins , total_spin, Objects }
-
-#total_spin = [spin_of_object0 , spin_of_object1]
-
-#Objects = [Object0, Object1 ]
-
-#Object = { spin_proj , states }
-
-#states = [state0  state1 state2]
-
-#state = {coefficients, bases}
-
-#bases = [base0 , base1 , base2]
-#coefficients = [coefficient0 , coefficient1 , coefficient2]
-
-#coefficient0 = 1
-#base0 = [+1/2 , +3/2 , -1/2 ,0]
-
-#actual state -->      Coefficient[0] bases[0]
-
-Total = {'small_spins': [0.5, 0.5, 0.5], 'total_spin': [], 'Objects': [{'spin_proj': [], 'states': [{'coefficients': [1], 'bases': [[0.5, 0.5, 0.5]]}]}]}
-
-Total = {'small_spins': [1, 0.5], 'total_spin': [1.5], 'Objects': [{'spin_proj': [], 'states': [{'coefficients': [1], 'bases': [[1, 0.5]]}]}]}
-
-#Total = {'small_spins': [1, 0.5], 'Objects': [{'states': [{1.5: [{'coefficients': [1], 'bases': [[1, 0.5]]}]}] , 'total_spin': 1.5}]} ###second idea for structure
+class Base:
+    def __init__(self, coefficient, base ,ind_spins):
+        self.base = base
+        self.coefficient = coefficient
+        self.ind_spins = ind_spins
 
 
-def Lowering(small_spins , State):
-    new_state = {'coefficients': [] , 'bases' : [] }
-    for i, base in enumerate(State['bases']):  # up up up up
+    def add_base(self, coefficient, base):
+        self.base.append(base)
+        self.coefficient.append(coefficient)
+
+class State:
+
+    def __init__(self, bases=None, ind_spins=None):
+
+        self.bases = bases
+        self.ind_spins = ind_spins
+
+        if bases != None:
+            self.spin = self.get_spin_proj()
+            self.ind_spins = bases[0].ind_spins
+        else:
+            self.bases = []
+
+    def get_spin_proj(self):
+        print(self.bases[0].base)
+        return sum(self.bases[0].base)
 
 
-        for index  in range(len(base)):
+    def add_dimension(self, new_coef, new_base):
+        try:
+
+            if sum(new_base) != self.spin:
+                return
+        except:
+            pass
+
+        try:
+            for base in self.bases:
+                if base.base == new_base:
+                    base.coefficient += new_coef
+                    return
+        except:
+            pass
+
+        if new_base == [] or new_coef == 0:
+            return
+
+        self.bases.append(Base(new_coef , new_base , self.ind_spins))
+        self.spin = self.get_spin_proj()
+
+
+    def __repr__(self):
+        s1 = 'Spin {}\n'.format(self.spin)
+        s2 =  str([ (' Coefficient: ' + repr(base.coefficient) + ' Bases: ' + repr(base.base)) for base in self.bases]) + "\n\n"
+        #s3 = '\n Bases:' + repr(self.bases)
+
+        return s1 + s2 #+ s3
+
+
+class Object:
+
+    def __init__(self, states , spin ):
+
+        self.states = {s.spin: s for s in states}
+        self.spin = spin
+
+    def fill(self):
+        try:
+            new_state = self.states[self.spin]
+            while new_state != None:
+                new_state = Lowering(new_state)
+                if new_state != None:
+                    self.states[new_state.spin] = new_state
+        except:
+            print("Couldn't get state with highest spin")
+
+def Lowering(state):
+    if state == None:
+        return
+
+    new_state = State( None , state.ind_spins )
+
+    for base in state.bases:
+        for index  in range(len(base.base)):
             new_base = []
-            print(str(index) + "at second loop")
-            j = small_spins[index]
-            for second_index , spin in enumerate(base):
-                print(str(second_index) + "at third loop")
+            j = state.ind_spins[index]
+            for second_index , spin in enumerate(base.base):
                 if second_index == index:
-                    if spin == -(small_spins[index]):
+                    if spin == -(state.ind_spins[index]):
                         new_base = []
-                        print("break")
+                        new_coeff = 0
                         break
                     else:
                         new_base.append(spin - 1)
-                        print("lowered ")
-                        print(new_base)
-                        new_coeff = State['coefficients'][i]*sqrt(j*(j+1)-spin*(spin-1))
+                        new_coeff = base.coefficient*sqrt(Rational(j*(j+1)-spin*(spin-1)))
                 else:
-                    print("not lowered ")
-                    print(new_base)
                     new_base.append(spin)
 
-            if new_base == []:
-                pass
-            elif new_base in new_state['bases']:
-                for k, same_base in enumerate(new_state['bases']):
-                    print("same_base")
-                    print(same_base)
-                    print(new_base)
-                    if same_base == new_base:
-                        print("#"*50)
-                        print(new_state['coefficients'][k])
-                        print(new_coeff)
-                        new_state['coefficients'][k] += new_coeff
-            else:
-                new_state['bases'].append(new_base)
-                new_state['coefficients'].append(new_coeff)
-    print(new_state)
+
+            new_state.add_dimension( new_coeff, new_base)
+
+    if new_state.bases == []:
+        return
+
+    #new_state.normalize()
     return new_state
 
-def equiv(Object , Object2):
-    pass
+def InnerProduct(state1 , state2):
+    prod = 0
+    for base in state1.bases:
+        for same_base in state2.bases:
+            if base.base == same_base.base:
+                prod += base.coefficient*same_base.coefficient
+
+    return prod
 
 
-nam = Lowering( Total['small_spins'] ,   Total['Objects'][0]['states'][0])
-nam2 = Lowering( Total['small_spins'] ,   nam)
-nam3 = Lowering( Total['small_spins'] ,   nam2)
-            #    new_base.append()
-        #for spin in base:
+
+states = [State([Base(1 ,[1, 1/2] , [1, 1/2] )] ) ,State([Base(1 ,[-1, -1/2] , [1, 1/2] )] ) ]
+
+obj = Object(states , 1.5)
+
+nam = State([Base(1 ,[1, 1/2] , [1, 1/2])])
