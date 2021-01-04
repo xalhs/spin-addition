c248133751236e07f1ce736ef4e79984a04a1861
xalhs <>
2021-01-04 06:13:32 +0200

    add orthogonalizing function and function that produces all states from the addition of two spins

--- spin_addition.py
+++ spin_addition.py
@@ -9,15 +9,26 @@ from sympy import*
 
 class Base:
     def __init__(self, coefficient, base ,ind_spins):
+        if coefficient == 0:
+            return
         rat_base = [Rational(x) for x in base]
         self.base = rat_base
         self.coefficient = coefficient
-        self.ind_spins = ind_spins
+        self.ind_spins = [Rational(y) for y in ind_spins]
 
 
     def add_base(self, coefficient, base):
         self.base.append(base)
         self.coefficient.append(coefficient)
+
+
+#obj.states[Rational(3/2)]
+
+#obj[1/2]
+#obj[3/2]
+
+#obj.states[1/2].bases[]
+#obj.states[3/2]
 
 class State:
 
@@ -34,7 +45,6 @@ class State:
             self.bases = []
 
     def get_spin_proj(self):
-        print(self.bases[0].base)
         return sum(self.bases[0].base)
 
     def get_total_spin(self):
@@ -44,10 +54,17 @@ class State:
         if new_state == None:
             new_state = State(None , self.ind_spins)
         for base in self.bases:
-            new_state.add_dimension(Rational(self.spin*(self.spin +1)) , base.base  )
+            new_state.add_dimension(Rational(self.spin*(self.spin +1))*base.coefficient , base.base  )
 
         prod =  InnerProduct(self , new_state)
         return ( (-1 + sqrt(1 + 4* prod))/2  )
+
+    def remove_dimension (self, remove_base):
+        for i , base in enumerate(self.bases):
+            if base.base== remove_base:
+                self.bases.pop(i)
+                return
+
 
     def add_dimension(self, new_coef, new_base):
         try:
@@ -61,6 +78,8 @@ class State:
             for base in self.bases:
                 if base.base == new_base:
                     base.coefficient += new_coef
+                    if base.coefficient == 0:
+                        self.remove_dimension(base.base)
                     return
         except:
             pass
@@ -76,12 +95,11 @@ class State:
         global_coef = 0
         for base in self.bases:
             coef = base.coefficient
-            print(coef)
             global_coef += coef*coef
-            print(global_coef)
 
         for base in self.bases:
             base.coefficient *= sqrt(global_coef)/global_coef
+
 
 
     def __repr__(self):
@@ -99,6 +117,9 @@ class Object:
         self.states = {s.spin: s for s in states}
         self.spin = states[0].get_total_spin()
         #self.spin = spin
+
+    def __getitem__(self, key):
+        return self.states[Rational(key)]
 
     def fill(self):
         try:
@@ -170,6 +191,64 @@ def Lowering(state):
     #new_state.normalize()
     return new_state
 
+def Orthogonal(States):
+    new_state = State( None , States[0].ind_spins )
+    A = [[0 for x in range(len(States) +1 )] for y in range(len(States) +1)]
+    spin = States[0].spin
+    for state in States:
+
+        #A.append([])
+        if len(state.bases) > len(States) + 1:
+            print("ERROR: can't orthogonalize states since more bases than states were given")
+            return
+
+    all_bases = [] ########make a list with all bases
+    temp_state = State([Base(1 , States[0].ind_spins , States[0].ind_spins)] )
+    for l in range(sum(States[0].ind_spins) - spin):
+        temp_state = Lowering(temp_state)
+
+    all_bases = [x.base for x in temp_state.bases]
+    if len(all_bases) != len(States) + 1:
+        return
+    for i, base in enumerate(all_bases):      #States[0]['bases']):
+        A[0][i] = base
+        for j , state in enumerate(States):
+            for state_base in state.bases:
+                if state_base.base == base:
+                    A[j+1][i] = state_base.coefficient
+
+    for i , base in enumerate(all_bases):    ##########################
+        B=[]
+        for j in range(1, len(A)):
+            B.append([])
+            for k in range(len(A)):
+                if k == i:
+                    continue
+                else:
+
+                    B[j-1].append(A[j][k])
+        new_state.add_dimension( ((-1)**i) * Matrix(B).det(), base)
+
+    return new_state
+
+def two_spin_adder(ind_spin1 , ind_spin2):
+    ind_spin1 = Rational(ind_spin1)
+    ind_spin2 = Rational(ind_spin2)
+    Total = {'ind_spins': [ind_spin1 , ind_spin2] , 'Objects': []}
+    for i in range(floor(ind_spin1 + ind_spin2) + 1):
+        orth_states = []
+        for j in range(i):
+
+            orth_states.append(Total['Objects'][j].states[ind_spin1 + ind_spin2 - i])
+        if i == 0:
+            obj = Object([State([Base(1 , [ ind_spin1 , ind_spin2 ] , [ ind_spin1 , ind_spin2 ]  )])])
+        else:
+            obj = Object([Orthogonal(orth_states)])
+        obj.fill()
+        Total['Objects'].append(obj)
+
+    return Total
+
 def InnerProduct(state1 , state2):
     prod = 0
     for base in state1.bases:
@@ -186,3 +265,9 @@ states = [State([Base(1 ,[1, 1/2] , [1, 1/2] )] ) ,State([Base(1 ,[-1, -1/2] , [
 obj = Object(states)
 
 nam = State([Base(1 ,[1, 1/2] , [1, 1/2])])
+
+tot =  two_spin_adder(3/2 , 3/2)
+
+for object1 in tot['Objects']:
+    print(object1.states)
+    print("\n\n")
